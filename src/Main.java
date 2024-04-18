47d00f947ae4836a81110ab60c077c2dbba696f0
TobyQAQ <a0988065874@gmail.com>
2024-04-18 13:51:06 +0800

    240418_1
    1.建立了二維陣列
    2.讀取它的長度

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] a = new int[][]{{2,3,4,5}, {8,8,4,3}, {4,6,8,4}};
+        System.out.println((a.length));
+        System.out.println( .length);
+
+    }
+
+}
