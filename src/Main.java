7e9e517bac758c775d38ebde5264b24dc645a548
TobyQAQ <a0988065874@gmail.com>
2024-04-18 14:04:50 +0800

    240418
    1.巢狀迴圈
    2.列印陣列

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[][] a = new int[][]{{2,3,4,5}, {8,8,4,3}, {4,6,8,4}};
+        int[][] a = new int[][]{{2, 3, 4, 5}, {8, 8, 4, 3}, {4, 6, 8, 4}};
         System.out.println((a.length));
-        System.out.println( .length);
+        System.out.println(a[0].length);
 
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+
+                System.out.print(a[i][j] + ",");
+
+            }
+            System.out.println();
+        }
     }
-
 }
